a67568fd3e5ace4931f6982577c07f027282994a
dipak-01 <imt_2022037@iiitm.ac.in>
2024-01-26 04:06:30 +0530

    updates

---
+++ src/components/MainPage.jsx
@@ -0,0 +1,15 @@
+import NavbarWithMegaMenu from "./Navbar"
+
+export default function MainPage() {
+    {
+
+
+        return (
+            <>
+                <NavbarWithMegaMenu />
+            </>
+
+        )
+
+    }
+}
